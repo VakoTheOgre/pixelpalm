869df310620c208ced83466d993b7a32c2645a6f
VakoTheOgre <keemo.sab3@gmail.com>
2019-05-14 23:31:04 +0300

    checkout

--- hello-world/src/components/navigation/tabs/CartTab.vue
+++ hello-world/src/components/navigation/tabs/CartTab.vue
@@ -67,24 +67,11 @@ export default {
       </div>
       <checkout-btn backColor="red" margin="3"></checkout-btn>
 
-      <span class="subtotal">SUBTOTAL: ${{ subtotal }}.00 </span>
-      <span class="total">ORDER TOTAL: ${{ subtotal }}.00 </span>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.subtotal {
-  padding-top: 4rem;
-  width: 100%;
-  text-align: left;
-  font-size: 1.6rem;
-}
-.total {
-  padding-top: 1rem;
-  width: 100%;
-  text-align: left;
-  font-size: 1.6rem;
-}
+
 
 .heading {
   width: 27rem;
---
+++ hello-world/src/components/navigation/tabs/SubtotalCheckout.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    computed: {
+        subtotal() {
+        return this.$store.getters['cart/subtotal']
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="subtotal-checkout-root flex-col">
+        <span class="subtotal">SUBTOTAL: ${{ subtotal }}.00 </span>
+        <span class="total">ORDER TOTAL: ${{ subtotal }}.00 </span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.subtotal-checkout-root {
+    border-top: 0.2rem solid black;
+    padding-top: 4rem;
+}
+.subtotal {
+  width: 100%;
+  text-align: left;
+  font-size: 1.6rem;
+}
+.total {
+  padding-top: 1rem;
+  width: 100%;
+  text-align: left;
+  font-size: 1.6rem;
+}
+</style>
